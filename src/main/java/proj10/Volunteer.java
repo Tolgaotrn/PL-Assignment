b5c82fbbd4cdb5371cc490716c4de281f3e7bcab
tolga oturan <tolgamurat.oturan@student.ucll.be>
2024-11-03 19:27:42 +0000

    volunteer class

---
+++ src/main/java/proj10/Volunteer.java
@@ -0,0 +1,31 @@
+package proj10;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.Date;
+
+@Entity
+@Table(name = "Volunteers")
+public class Volunteer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "phone", nullable = false)
+    private String phone;
+
+    @Column(name = "date_joined", nullable = false)
+    private Date dateJoined;
+
+
+}
